ea3a6d7e3da2e78945636b8b8d1e82868e8f208b
Random Guy <leekhoi5319990@gmail.com>
2022-03-30 17:05:10 +0700

    03-Example of a LOCALIZED CONTEXT

--- src/final/03.js
+++ src/final/03.js
@@ -32,7 +32,7 @@ function ToggleOff({children}) {
 	return on ? null : children
 }
 
-function ToggleButton({...props}) {
+function ToggleButton(props) {
 	const {on, toggle} = useToggle()
 	return <Switch on={on} onClick={toggle} {...props} />
 }
