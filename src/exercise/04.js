3b02e1c9873af62883ba6b54e6eece05a8434815
Random Guy <leekhoi5319990@gmail.com>
2022-03-31 16:40:26 +0700

    04-x.1_Returning composed props as function arguments

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,9 +1,9 @@
 // Prop Collections and Getters
 // http://localhost:3000/isolated/exercise/04.js
+// Checkout '../final/04.extra-1' for the solution of Extra Credit
 
 import * as React from 'react'
 import {Switch} from '../switch'
-
 function useToggle() {
 	const [on, setOn] = React.useState(false)
 	const toggle = () => setOn(!on)
@@ -11,18 +11,30 @@ function useToggle() {
 	// 🐨 Add a property called `togglerProps`. It should be an object that has
 	// `aria-pressed` and `onClick` properties.
 	// 💰 {'aria-pressed': on, onClick: toggle}
-	const togglerProps = {'aria-pressed': on, onClick: toggle}
+	const getTogglerProps = props => {
+		return {
+			'aria-pressed': on,
+			onClick: toggle,
+			...props,
+		}
+	}
 
-	return {on, togglerProps}
+	return {on, toggle, getTogglerProps}
 }
 
 function App() {
-	const {on, togglerProps} = useToggle()
+	const {on, getTogglerProps} = useToggle()
 	return (
 		<div>
-			<Switch on={on} {...togglerProps} />
+			<Switch {...getTogglerProps({on})} />
 			<hr />
-			<button aria-label="custom-button" {...togglerProps}>
+			<button
+				{...getTogglerProps({
+					'aria-label': 'custom-button',
+					onClick: () => console.info('onButtonClick'),
+					id: 'custom-button-id',
+				})}
+			>
 				{on ? 'Turn it off' : 'Turn it on'}
 			</button>
 		</div>
--- src/final/04.extra-1.js
+++ src/final/04.extra-1.js
@@ -5,8 +5,14 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+// `...fns` is get all its arguments (as FUNCTIONS)
 const callAll =
 	(...fns) =>
+	// then for each argument (function), return their executed value with all the arguments (`...args`) that they take
+	// `...args` is just for general purpose,
+	// in this case, without the `...args` it still works
+	// coz `toggle` and `() => console.info('onButtonClick')` receive no argument
+	// `fn?.(...args)` so that it executes the `() => console.info('onButtonClick')` from the <button/> (if that's passed in), if there's no `?.` then if you clicked on the <Switch/>, as there's no `onClick` prop passed in, and it assumes there's an `onClick` function, it'll go and execute that non-existing function, causing the "TypeError: fn is not a function", but since the `toggle` is still a default return value from the `getTogglerProps()`, the <Switch/> still works if you click on the <button/>
 	(...args) =>
 		fns.forEach(fn => fn?.(...args))
 
@@ -17,7 +23,13 @@ function useToggle() {
 	function getTogglerProps({onClick, ...props} = {}) {
 		return {
 			'aria-pressed': on,
+			// in this case:
+			// onClick: toggle() -- for the <Switch/>
+			// onClick: (() => console.info('onButtonClick'))() -- for the <button/>
 			onClick: callAll(onClick, toggle),
+			// in this case:
+			// `id: 'custom-button-id'` for the <button/>
+			// destructured `{on}` for the <Switch/>
 			...props,
 		}
 	}
@@ -36,11 +48,14 @@ function App() {
 			<Switch {...getTogglerProps({on})} />
 			<hr />
 			<button
-				{...getTogglerProps({
-					'aria-label': 'custom-button',
-					onClick: () => console.info('onButtonClick'),
-					id: 'custom-button-id',
-				})}
+				{
+					// spread to get all the returned props from the execution of getTogglerProps()
+					...getTogglerProps({
+						'aria-label': 'custom-button',
+						onClick: () => console.info('onButtonClick'),
+						id: 'custom-button-id',
+					})
+				}
 			>
 				{on ? 'on' : 'off'}
 			</button>
